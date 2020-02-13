5eb782b036aee115e5cc2ddd6aa7b42497b15fe8
Lord-Fifth <aditya.suresh098@gmail.com>
2020-02-13 23:51:57 +0530

    TCS Xplore

---
+++ Atoms & Molecules/TCS/Xplore/OddPosition.java
@@ -0,0 +1,23 @@
+/*
+To print characters at odd position of a string
+
+
+input: Matrix
+output : Mti
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class OddPosition {
+
+    public static void main(String args[])throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        for(int i = 0; i < str.length(); i++) {
+            if(i % 2 == 0)
+                System.out.print(str.charAt(i));
+        }
+    }
+}
