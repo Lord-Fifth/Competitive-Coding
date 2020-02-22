05b7a8e07f797a06820dfcfab370fbc80d986bb5
Lord-Fifth <aditya.suresh098@gmail.com>
2020-02-22 19:39:43 +0530

    TCS Xplore

---
+++ Atoms & Molecules/TCS/Xplore/Factorial.java
@@ -0,0 +1,52 @@
+import java.io.*;
+/*
+Write a program to read 5 numbers and print factorials of each.
+(Final answers should be non decimal numbers).
+*/
+
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Factorial {
+    public static void main(String args[] ) throws Exception {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        int a[] = new int[5];
+        int f[] = new int[5];
+
+        for(int i = 0; i < 5; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+            f[i] = fact(a[i]);
+        }
+
+        for(int i = 0; i < 5; i++) {
+            System.out.println(f[i]);
+        }
+
+    }    
+
+    public static int fact(int n) {
+        if(n == 1) 
+            return 1;
+        else    
+            return (n * fact(n - 1));
+    }
+}
+
+/*
+Input:
+2
+3
+4
+6
+5
+
+Ouput:
+2
+6
+24
+720
+120
+*/
